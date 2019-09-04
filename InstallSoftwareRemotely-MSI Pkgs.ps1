2344faf3d7bf7fe58d76d98ac8dc99ac448823d3
Andrewkasaija <53442572+Andrewkasaija@users.noreply.github.com>
2019-09-04 08:57:46 +0300

    Add files via upload

---
+++ InstallSoftwareRemotely-MSI Pkgs.ps1
@@ -0,0 +1,19 @@
+﻿$computers = 'Enter Computer Name'
+$sourcefile = "Source file for the MSI"
+
+$jobscript = {
+	Param($computer)
+	$destinationFolder = "\\$computer\C$\Temp"
+
+	if (!(Test-Path -path $destinationFolder))
+ {
+		New-Item $destinationFolder -Type Directory
+	}
+	Copy-Item -Path $sourcefile -Destination $destinationFolder
+	Invoke-Command -ComputerName $computer -ScriptBlock { Msiexec Enter location path of MSI FILE /i  /log C:\MSIInstall.log }
+}
+
+$computer | 
+	ForEach-Object{
+		Start-Job -ScriptBlock $jobscript -ArgumentList $_ -Credential $domaincredentail
+	}
